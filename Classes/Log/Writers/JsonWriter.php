67e10a50e6d8615ded16dea686b21aa51b298994
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-05-30 18:30:58 +0200

    chore: remove dead code

--- Classes/Log/Writers/JsonWriter.php
+++ Classes/Log/Writers/JsonWriter.php
@@ -2,7 +2,6 @@
 
 namespace Jops\TYPO3\Loki\Log\Writers;
 
-use Throwable;
 use RuntimeException;
 use TYPO3\CMS\Core\Log\LogRecord;
 use TYPO3\CMS\Core\Log\Writer\FileWriter;
@@ -11,20 +10,7 @@ class JsonWriter extends FileWriter
 {
     public function writeLog(LogRecord $record)
     {
-        $data = '';
-        $context = $record->getData();
-        $message = $record->getMessage();
-        if (!empty($context)) {
-            // Fold an exception into the message, and string-ify it into context so it can be jsonified.
-            if (isset($context['exception']) && $context['exception'] instanceof Throwable) {
-                $message .= $this->formatException($context['exception']);
-                $context['exception'] = (string)$context['exception'];
-            }
-            $data = $context;
-        }
-
         $message = json_encode($record->toArray(), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-
 
         if (false === fwrite(self::$logFileHandles[$this->logFile], $message . LF)) {
             throw new RuntimeException('Could not write log record to log file', 1345036335);
